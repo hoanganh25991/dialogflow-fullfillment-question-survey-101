d8973c0437bb7630f7e54c6c4a5914928a496d03
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-11-15 11:53:54 +0700

    Save log in file not works with cloud function

--- index.js
+++ index.js
@@ -1,30 +1,25 @@
 "use strict"
 // https://us-central1-glass-turbine-148103.cloudfunctions.net/questionSurvey101
 const http = require("http")
+const fs = require("fs")
 exports.askQuestion = (req, res) => {
   const reqParams = req.body.result
   // const reqStr = JSON.stringify(req.body.originalRequest, null, 2)
   const reqStr = JSON.stringify(req.body, null, 2)
   res.setHeader("Content-Type", "application/json")
 
+  const answerLogPath = `${__dirname}/answer.log`
+  const hasFile = fs.existsSync(answerLogPath)
+  const lastLogContent = hasFile ? fs.readFileSync(answerLogPath).toString() : ""
+
+  const sessionId = req.body.sessionId
+
+  const currTimestamp = new Date().getTime()
+  const content = lastLogContent + JSON.stringify({ [currTimestamp]: sessionId })
+
   res.send(
     JSON.stringify({
-      speech: "Default speech",
-      messages: [
-        {
-          buttons: [
-            {
-              postback: "Card Link URL or text",
-              text: "Card Link Title"
-            }
-          ],
-          imageUrl: "https://tinker.press/images/favicon.png",
-          platform: "facebook",
-          subtitle: "Card Subtitle",
-          title: "Card Title",
-          type: 1
-        }
-      ]
+      speech: content
     })
   )
 }
