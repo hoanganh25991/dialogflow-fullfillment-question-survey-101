f09d92a7643f77c16c549dea95936d75251e5834
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-11-15 13:18:48 +0700

    Get next question workflow

--- index.js
+++ index.js
@@ -1,25 +1,86 @@
 "use strict"
 // https://us-central1-glass-turbine-148103.cloudfunctions.net/questionSurvey101
-const http = require("http")
-const fs = require("fs")
+const getAnswersSession = sessionId => [
+  {
+    _id: "xxx",
+    questionId: "123456789",
+    // Place question info here
+    order: 1,
+    answer: {
+      text: "iOS",
+      img_url: "http://by.originally.us/howmuch/images/iphone-300x300.png",
+      pay: 0
+    }
+  },
+  {
+    _id: "yyy",
+    questionId: "987654321",
+    order: 2,
+    answer: {
+      text: "tablet",
+      img_url: "http://by.originally.us/howmuch/images/iphone-300x300.png",
+      pay: 0
+    }
+  }
+]
+
+const apiNextQuestion = () => ({
+  _id: "123456789",
+  order: 5,
+  text: "Which platforms do you wish to build for?",
+  answers: [
+    {
+      text: "iOS",
+      img_url: "http://by.originally.us/howmuch/images/iphone-300x300.png",
+      pay: 10
+    },
+    {
+      text: "Android",
+      img_url: "http://by.originally.us/howmuch/images/android-300x300.png",
+      pay: 10
+    },
+    {
+      text: "Web app",
+      img_url: "http://by.originally.us/howmuch/images/browser-300x300.png",
+      pay: 10
+    }
+  ]
+})
+
+const getQuestion = sessionId => {
+  // Find user answer in history
+  const answersSession = getAnswersSession(sessionId)
+  const answerWithHighestOrder = answersSession.sort((a, b) => a.order < b.order)[0]
+  const currOrder = answerWithHighestOrder ? answerWithHighestOrder.order : 1
+  const questionIds = answersSession.map(answer => answer.questionId)
+
+  console.log(currOrder, questionIds)
+
+  return apiNextQuestion({ order: currOrder + 1, questionIds })
+}
+
 exports.askQuestion = (req, res) => {
-  const reqParams = req.body.result
-  // const reqStr = JSON.stringify(req.body.originalRequest, null, 2)
-  const reqStr = JSON.stringify(req.body, null, 2)
   res.setHeader("Content-Type", "application/json")
-
-  const answerLogPath = `${__dirname}/answer.log`
-  const hasFile = fs.existsSync(answerLogPath)
-  const lastLogContent = hasFile ? fs.readFileSync(answerLogPath).toString() : ""
-
   const sessionId = req.body.sessionId
+  let question = getQuestion(sessionId)
 
-  const currTimestamp = new Date().getTime()
-  const content = lastLogContent + JSON.stringify({ [currTimestamp]: sessionId })
+  if (req.body.result.resolvedQuery === "end") question = null
 
-  res.send(
-    JSON.stringify({
-      speech: content
-    })
-  )
+  let resObj = {}
+
+  if (!question) {
+    // Should jump to summary
+    resObj = {
+      ...resObj,
+      contextOut: [{ name: "Summary", lifeSpan: 1 }],
+      speech: "Go to summary"
+    }
+  } else {
+    const { text: questionStr, answers } = question
+    const defaultAnswer = answers.reduce((carry, answer) => `${carry}/${answer.text}`, "Next/Previous")
+    const defaultSpeech = `${questionStr}\n${defaultAnswer}`
+    resObj = { ...resObj, speech: defaultSpeech }
+  }
+
+  res.send(JSON.stringify(resObj))
 }
